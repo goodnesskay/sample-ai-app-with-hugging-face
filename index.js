fcbda0da5de2c4bd1dc04e962d5919f79a0447fa
Goodness Kayode <gtkbrain@gmail.com>
2024-06-27 13:21:43 +0000

    add text to image model

--- index.js
+++ index.js
@@ -28,6 +28,25 @@ app.post('/api/v1/generate', async (req, res) => {
     }
 });
 
+app.post('/api/v1/image/generate', async (req, res) => {
+    const inputText = req.body.text;
+
+    try {
+        const response = await axios.post(MODEL_URL+'/CompVis/stable-diffusion-v1-4', {
+            inputs: inputText,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${HUGGING_FACE_API_TOKEN}`
+            }
+        });
+
+        res.send(response.data);
+    } catch (error) {
+        console.error('Error calling the Hugging Face API:', error);
+        res.status(500).send('An error occurred', error);
+    }
+});
+
 app.get('/', async (req, res) => {
     res.send("Welcome to the Sample API with Hugging Face");
 });
