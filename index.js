49b5bf6c1867cc8b9ba6f92d4db2d55f96b9cf68
Goodness Kayode <gtkbrain@gmail.com>
2024-06-27 13:10:33 +0000

    include error in request

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/api/v1/generate', async (req, res) => {
     const inputText = req.body.text;
 
     try {
-        const response = await axios.post(MODEL_URL+'/meta-llama/Llama-2-70b-hf', {
+        const response = await axios.post(MODEL_URL+'/HuggingFaceH4/zephyr-7b-beta', {
             inputs: inputText,
         }, {
             headers: {
