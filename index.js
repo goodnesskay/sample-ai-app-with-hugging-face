915b95fd83d0538d7d623f799f7e4188dcabcd50
Goodness Kayode <gtkbrain@gmail.com>
2024-07-04 15:23:39 +0000

    generate music lyrics

--- index.js
+++ index.js
@@ -49,6 +49,25 @@ app.post('/api/v1/image/generate', async (req, res) => {
     }
 });
 
+app.post('/api/v1/music/generate', async (req, res) => {
+    const inputText = req.body.text;
+
+    try {
+        const response = await axios.post(MODEL_URL+'/HeyLucasLeao/gpt-neo-small-emo-lyrics', {
+            inputs: inputText,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${HUGGING_FACE_API_TOKEN}`
+            }
+        });
+
+        res.send(response.data);
+    } catch (error) {
+        console.error('Error calling the Hugging Face API:', error);
+        res.status(500).send('An error occurred', error);
+    }
+});
+
 app.get('/', async (req, res) => {
     res.send("Welcome to the Sample API with Hugging Face");
 });
